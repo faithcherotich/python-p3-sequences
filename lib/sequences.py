c6cff69de3479fb35ebf8bd9a23c73bc84814608
faithcherotich <faithcherotich910@gmail.com>
2024-08-29 21:53:03 +0300

    first commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return[]
+    fibonacci = [0, 1]
+    while len(fibonacci) < length:
+        next_value = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_value)
+    
+    print(fibonacci[:length]) 
